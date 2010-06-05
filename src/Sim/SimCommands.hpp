b5714ab8725e095138d4c4fd3f9e15d5b3d635d4
kloot <kloot@kloot0.(none)>
2010-06-05 13:48:58 +0200

    specify types

--- src/Sim/SimCommands.hpp
+++ src/Sim/SimCommands.hpp
@@ -2,9 +2,9 @@
 #define PFFG_SIMCOMMANDS_HDR
 
 enum CommandType {
-	COMMAND_CREATE_SIMOBJECT  = 0, // objectDefID, objectPos
-	COMMAND_DESTROY_SIMOBJECT = 1, // objectID (1)
-	COMMAND_MOVE_SIMOBJECT    = 2, // goalPos (1), objectID (>= 1)
+	COMMAND_CREATE_SIMOBJECT  = 0, // uint objectDefID, vec3f objectPos
+	COMMAND_DESTROY_SIMOBJECT = 1, // uint objectID (1)
+	COMMAND_MOVE_SIMOBJECT    = 2, // vec3f goalPos (1), uint objectID (>= 1)
 	COMMAND_LAST              = 3,
 };
 
