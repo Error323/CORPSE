4de5341d358ddf3c9662f106a5daa7acf6a6b816
Error323 <folkerthuizinga@gmail.com>
2010-06-06 13:52:19 +0200

    Implemented cmake buildsystem, removed makefile

--- src/System/Main.cpp
+++ src/System/Main.cpp
@@ -1,6 +1,10 @@
+#include <iostream>
+
 #include "./Engine.hpp"
+#include "./Corpse.hpp"
 
 int main(int argc, char** argv) {
+	std::cout << HUMAN_NAME << std::endl;
 	CEngine* engine;
 
 	engine = CEngine::GetInstance(argc, argv);
