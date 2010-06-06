0285e734749f83f13ac3fbb3527c98fc416a3f00
kloot <kloot@kloot0.(none)>
2010-06-06 15:26:18 +0200

    remove unused sources

--- src/System/Main.cpp
+++ src/System/Main.cpp
@@ -1,10 +1,6 @@
-#include <iostream>
-
 #include "./Engine.hpp"
-#include "Corpse.hpp"
 
 int main(int argc, char** argv) {
-	std::cout << HUMAN_NAME << std::endl;
 	CEngine* engine;
 
 	engine = CEngine::GetInstance(argc, argv);
